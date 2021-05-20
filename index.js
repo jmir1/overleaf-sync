d70c0cae8e0e17579c46d0df51c71fcaf5dc4670
jmir1 <jhmiramon@gmail.com>
2021-05-20 15:38:15 +0200

    implement login and download of projects as zip.

--- index.js
+++ index.js
@@ -1,21 +1,159 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const https = require("https");
+const parser = require("node-html-parser");
+const scparser = require("set-cookie-parser");
+const fs = require("fs");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const email = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+    const start = new Date();
+    core.debug(new Date().toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    var logindata = await login(email, password);
+    var allprojects = await parse_projects(logindata.projects);
+    var activeprojects = allprojects.filter(
+      (project) => !(project.archived || project.trashed)
+    );
+    for (project in activeprojects) {
+      download_project(
+        activeprojects[project].id,
+        logindata.session,
+        logindata.gclb
+      );
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info(new Date().toTimeString());
+    const end = new Date();
+    const time = end - start;
+    core.setOutput("time", time);
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function login(email, password) {
+  //GET login page
+  const get = await get_login();
+  //get necessary info from response
+  const csrf = parser
+    .parse(get.html)
+    .querySelector(`meta[name="ol-csrfToken"]`)
+    .getAttribute("content");
+  const session1 = scparser
+    .parse(get.headers["set-cookie"], { decodeValues: false })
+    .find((cookie) => cookie.name == "overleaf_session2").value;
+  const gclb = scparser
+    .parse(get.headers["set-cookie"], { decodeValues: false })
+    .find((cookie) => cookie.name == "GCLB").value;
+
+  //POST login data
+  const post = await post_login(csrf, email, password, session1, gclb);
+  //get necessary data from response
+  const session2 = scparser
+    .parse(post["set-cookie"], { decodeValues: false })
+    .find((cookie) => cookie.name == "overleaf_session2").value;
+
+  //GET new csrf token from project page
+  const projects = await get_projects(session2, gclb);
+  const csrf2 = parser
+    .parse(projects.html)
+    .querySelector(`meta[name="ol-csrfToken"]`)
+    .getAttribute("content");
+
+  //return data
+  return {
+    session: session2,
+    gclb: gclb,
+    csrf: csrf2,
+    projects: projects.html
+  };
+}
+
+async function parse_projects(html) {
+  const projectstr = parser
+    .parse(html)
+    .querySelector("meta[name=ol-projects]")
+    .getAttribute("content");
+  return JSON.parse(projectstr);
+}
+
+async function download_project(project_id, session, gclb) {
+  const file = fs.createWriteStream(`${project_id}.zip`);
+  const options = {
+    headers: { Cookie: `overleaf_session2=${session}; GCLB=${gclb}` }
+  };
+  https.get(
+    `https://www.overleaf.com/project/${project_id}/download/zip`,
+    options,
+    function (response) {
+      response.pipe(file);
+    }
+  );
+}
+
+async function get_login() {
+  const url = "https://www.overleaf.com/login";
+  return new Promise((resolve) => {
+    https.get(url, (res) => {
+      var data;
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        resolve({ html: data, headers: res.headers });
+      });
+    });
+  });
+}
+
+async function get_projects(session2, gclb) {
+  const url = "https://www.overleaf.com/project";
+  return new Promise((resolve) => {
+    https.get(
+      url,
+      { headers: { Cookie: `GCLB=${gclb};overleaf_session2=${session2}` } },
+      (res) => {
+        var data;
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          resolve({ html: data, headers: res.headers });
+        });
+      }
+    );
+  });
+}
+
+async function post_login(_csrf, email, password, session1, gclb) {
+  const url = "https://www.overleaf.com/login";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `GCLB=${gclb};overleaf_session2=${session1}`
+    }
+  };
+  const postData = {
+    _csrf: _csrf,
+    email: email,
+    password: password
+  };
+  return new Promise((resolve) => {
+    var req = https.request(url, options, (res) => {
+      resolve(res.headers);
+    });
+
+    req.on("error", (e) => {
+      console.error(e);
+    });
+
+    req.write(JSON.stringify(postData));
+    req.end();
+  });
 }
 
 run();
